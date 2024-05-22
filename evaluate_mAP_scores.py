4efde604f51bfccabe6ccdbf6b8f3f844a5340ac
Thomas McCabe <tom.mccabe@missionsystems.com.au>
2024-05-22 11:10:51 +1000

    evaluate_mAP_scores.py: Changed print statements to log

--- evaluate_mAP_scores.py
+++ evaluate_mAP_scores.py
@@ -152,7 +152,7 @@ def calculate_mAP_scores(ground_truth_dir: str,
     logger.info("Calculating mAP Score")
     MINOVERLAP = 0.5  # default value (defined in the PASCAL VOC2012 challenge)
     ignore = ['tgt_box_11', 'tgt_box_3','tgt_craypot']
-    print(ignore)
+    log('Ignoring the following classes: {}'.format(ignore))
     #exit(1)
     if set_class_iou is not None:
         specific_iou_flagged = True if len(set_class_iou) > 0 else False
